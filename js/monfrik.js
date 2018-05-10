10509b5ffdf30414d94b21786be35ad1cc310880
AnastasiaLitvinovka <anastasija.sukhoveeva@yandex.ru>
2018-05-10 20:21:54 +0300

    add to js

--- js/monfrik.js
+++ js/monfrik.js
@@ -1,9 +1,68 @@
 window.onload = function () {
 
-var img_btn = document.querySelectorAll('img_btn');
-img_btn.forEach(function(i) {
-i.addEventListener('click', function(e) {
-e.target.classList.toggle('show');
-})
-})
+var img_btn1 = document.getElementById('img1'),
+img_btn2 = document.getElementById('img2'),
+img_btn3 = document.getElementById('img3'),
+img_btn4 = document.getElementById('img4'),
+img_btn5 = document.getElementById('img5'),
+img_btn6 = document.getElementById('img6'),
+img_btn7 = document.getElementById('img7'),
+img_btn8 = document.getElementById('img8'),
+img_btn9 = document.getElementById('img9'),
+img_btn10 = document.getElementById('img10'),
+img_btn11 = document.getElementById('img11');
+img_btn1.onclick = function (){
+    img_btn1.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+img_btn2.onclick = function (){
+    img_btn2.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+img_btn3.onclick = function (){
+    img_btn3.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+img_btn4.onclick = function (){
+    img_btn4.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+img_btn5.onclick = function (){
+    img_btn5.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+img_btn6.onclick = function (){
+    img_btn6.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+img_btn7.onclick = function (){
+    img_btn7.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+img_btn8.onclick = function (){
+    img_btn8.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+img_btn9.onclick = function (){
+    img_btn9.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+img_btn10.onclick = function (){
+    img_btn10.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+img_btn11.onclick = function (){
+    img_btn11.classList.toggle('show');
+    body.classList.toggle('body');
+    }
+
+var body = document.getElementById('body');
+//var img_btn1 = document.querySelectorAll('img_btn');
+//img_btn.forEach(function(i) {
+//i.addEventListener('click', function(e) {
+//e.target.classList.toggle('хер в вазилине')
+//})
+//})
+
+
 }
